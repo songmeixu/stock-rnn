802913769eb3a8fdd376ba8c1f9b34fc2297864b
Restyled.io <commits@restyled.io>
2021-01-28 03:18:13 +0000

    Restyled by black

--- data_model.py
+++ data_model.py
@@ -7,15 +7,17 @@ import time
 random.seed(time.time())
 
 
-class StockDataSet():
-    def __init__(self,
-                 stock_sym,
-                 input_size=1,
-                 num_steps=30,
-                 batch_size=64,
-                 test_ratio=0.1,
-                 normalized=True,
-                 close_price_only=True):
+class StockDataSet:
+    def __init__(
+        self,
+        stock_sym,
+        input_size=1,
+        num_steps=30,
+        batch_size=64,
+        test_ratio=0.1,
+        normalized=True,
+        close_price_only=True,
+    ):
         self.stock_sym = stock_sym
         self.input_size = input_size
         self.num_steps = num_steps
@@ -26,36 +28,45 @@ class StockDataSet():
 
         # Read csv file
         raw_df = pd.read_csv(os.path.join("data", "%s.csv" % stock_sym))
-        raw_df.sort_values(by='Date').reset_index()
+        raw_df.sort_values(by="Date").reset_index()
 
         # Merge into one sequence
         if close_price_only:
-            self.raw_seq = raw_df['Close'].tolist()
+            self.raw_seq = raw_df["Close"].tolist()
         else:
-            self.raw_seq = [price for tup in raw_df[[
-                'Open', 'Close']].values for price in tup]
+            self.raw_seq = [
+                price for tup in raw_df[["Open", "Close"]].values for price in tup
+            ]
 
         self.raw_seq = np.array(self.raw_seq)
         self.train_X, self.train_y, self.test_X, self.test_y = self._prepare_data(
-            self.raw_seq)
+            self.raw_seq
+        )
 
     def info(self):
         return "StockDataSet [%s] train: %d test: %d" % (
-            self.stock_sym, len(self.train_X), len(self.test_y))
+            self.stock_sym,
+            len(self.train_X),
+            len(self.test_y),
+        )
 
     def _prepare_data(self, seq):
         # split into items of input_size
-        seq = [np.array(seq[i * self.input_size: (i + 1) * self.input_size])
-               for i in range(len(seq) // self.input_size)]
+        seq = [
+            np.array(seq[i * self.input_size : (i + 1) * self.input_size])
+            for i in range(len(seq) // self.input_size)
+        ]
 
         if self.normalized:
             seq = [seq[0] / seq[0][0] - 1.0] + [
-                curr / seq[i][-1] - 1.0 for i, curr in enumerate(seq[1:])]
+                curr / seq[i][-1] - 1.0 for i, curr in enumerate(seq[1:])
+            ]
 
         # split into groups of num_steps
-        self.X_y = np.array([seq[i: i + self.num_steps+1]
-                             for i in range(len(seq) - self.num_steps)])
-        X = self.X_y[:, :self.num_steps, :]
+        self.X_y = np.array(
+            [seq[i : i + self.num_steps + 1] for i in range(len(seq) - self.num_steps)]
+        )
+        X = self.X_y[:, : self.num_steps, :]
         y = self.X_y[:, self.num_steps, :]
 
         train_size = int(len(X) * (1.0 - self.test_ratio))
@@ -69,7 +80,7 @@ class StockDataSet():
 
         np.random.shuffle(self.X_y)
         for j in range(num_batches):
-            batch_X = self.train_X[j * batch_size: (j + 1) * batch_size]
-            batch_y = self.train_y[j * batch_size: (j + 1) * batch_size]
+            batch_X = self.train_X[j * batch_size : (j + 1) * batch_size]
+            batch_y = self.train_y[j * batch_size : (j + 1) * batch_size]
             assert set(map(len, batch_X)) == {self.num_steps}
             yield batch_X, batch_y
